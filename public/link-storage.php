8ee67bdd3c07f869a5c416c3696c328154157d0d
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-04-15 23:06:48 +0200

    feat: add script to manually create storage symlink for shared hosting environments

--- public/link-storage.php
+++ public/link-storage.php
@@ -5,10 +5,18 @@ $link = __DIR__ . '/storage';
 
 if (file_exists($link)) {
     echo "The 'storage' link already exists.";
+} elseif (!function_exists('symlink')) {
+    echo "ERROR: The 'symlink' function is disabled on your hosting.<br><br>";
+    echo "<b>Alternative Solution:</b><br>";
+    echo "1. Log in to your cPanel/Hosting Control Panel.<br>";
+    echo "2. Find 'Cron Jobs'.<br>";
+    echo "3. Add a new job to run <b>once</b> with this command:<br>";
+    echo "<code>ln -s $target $link</code><br><br>";
+    echo "4. After it runs, your images will appear!";
 } else {
-    if (symlink($target, $link)) {
+    if (@symlink($target, $link)) {
         echo "Congratulations! The 'storage' link has been created successfully.";
     } else {
-        echo "Failed to create the link. You may need to ask your hosting support to create a symlink from $target to $link.";
+        echo "Failed to create the link manually. Please contact your hosting support and ask them to create a symlink from $target to $link.";
     }
 }
