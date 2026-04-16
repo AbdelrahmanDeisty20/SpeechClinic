01e953ad6ea7fbdab4f29e2749d9979643669074
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-04-16 03:37:26 +0200

    feat: implement BookingRequest validation and add corresponding localization strings

--- app/Http/Requests/API/BookingRequest.php
+++ app/Http/Requests/API/BookingRequest.php
@@ -30,4 +30,26 @@ class BookingRequest extends FormRequest
             'problem_description' => 'required|string|min:10',
         ];
     }
+    public function messages(): array
+    {
+        return [
+            'available_time_id.required' => __('messages.available_time_required'),
+            'available_time_id.exists' => __('messages.available_time_invalid'),
+            'child_name.required' => __('messages.child_name_required'),
+            'child_name.string' => __('messages.child_name_string'),
+            'child_name.min' => __('messages.child_name_min'),
+            'child_name.max' => __('messages.child_name_max'),
+            'child_age.required' => __('messages.child_age_required'),
+            'child_age.integer' => __('messages.child_age_integer'),
+            'child_age.min' => __('messages.child_age_min'),
+            'child_age.max' => __('messages.child_age_max'),
+            'child_photo.required' => __('messages.child_photo_required'),
+            'child_photo.image' => __('messages.child_photo_image'),
+            'child_photo.mimes' => __('messages.child_photo_mimes'),
+            'child_photo.max' => __('messages.child_photo_max'),
+            'problem_description.required' => __('messages.problem_description_required'),
+            'problem_description.string' => __('messages.problem_description_string'),
+            'problem_description.min' => __('messages.problem_description_min'),
+        ];
+    }
 }
