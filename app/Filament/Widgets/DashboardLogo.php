68678d684b1daed1968d185647b567dd04f8f50d
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-04-17 14:08:57 +0200

    feat: add DashboardLogo widget to Filament dashboard

--- app/Filament/Widgets/DashboardLogo.php
+++ app/Filament/Widgets/DashboardLogo.php
@@ -6,7 +6,7 @@ use Filament\Widgets\Widget;
 
 class DashboardLogo extends Widget
 {
-    protected static string $view = 'filament.widgets.dashboard-logo';
+    protected string $view = 'filament.widgets.dashboard-logo';
 
     protected static ?int $sort = 0;
 
