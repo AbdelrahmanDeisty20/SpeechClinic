03ea87f41ba51dab6496d07f6c5274d37678b0f4
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-04-18 07:10:02 +0200

    feat: add BookinMonthlyResource for managing monthly booking packages and appointments

--- app/Filament/Resources/BookinMonthlyResource.php
+++ app/Filament/Resources/BookinMonthlyResource.php
@@ -34,7 +34,7 @@ class BookinMonthlyResource extends Resource
 
     protected static string|\UnitEnum|null $navigationGroup = 'Booking Management';
     
-    protected static ?int $navigationSort = 2;
+    protected static ?int $navigationSort = 1;
 
     public static function getNavigationGroup(): ?string
     {
