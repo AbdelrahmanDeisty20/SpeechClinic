cb8ed2032a663f98cd9ef6f1f861825e852c4dd2
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-05-14 20:09:36 +0300

    feat: create DayResource to transform day data with related available times and branch details

--- app/Http/Resources/API/DayResource.php
+++ app/Http/Resources/API/DayResource.php
@@ -17,7 +17,7 @@ class DayResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'available_times' => AvailableTimeResource::collection($this->availableTimes),
+            'available_times' => AvailableTimeResource::collection($this->whenLoaded('availableTimes')),
             'branch' => BranchResource::make($this->whenLoaded('branch')),
         ];
     }
